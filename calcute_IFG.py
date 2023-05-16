97da3eac7e469650f2c49fb122a9dfe953945ddf
Hongcan Li <lihongcanyuzuru@gmail.com>
2023-05-16 15:13:55 +0800

    Double loop removed

--- calcute_IFG.py
+++ calcute_IFG.py
@@ -1,5 +1,6 @@
 # function for calculating the interferogram from SLC data
 import h5py
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -11,42 +12,70 @@ def read_dset(hdf5_file_path, group_name):
     return dset
 
 
-def calculate_IFG(SLC_data):
-    master_name = '20220327'
+# def calculate_IFG(SLC_data, master_name):
+#     cal_IFG_data = {}
+#     for dset_name, dset_data in SLC_data.items():
+#             cal_IFG_data[dset_name] = np.empty_like(dset_data)
+#             for i in range(dset_data.shape[0]):
+#                 for j in range(dset_data.shape[1]):
+#                     cal_IFG_data[dset_name][i, j] = SLC_data[master_name][i, j] * np.conj(dset_data[i, j])
+#     print("IFG :", cal_IFG_data)
+#     return cal_IFG_data
+
+def calculate_IFG(SLC_data, master_name):
     cal_IFG_data = {}
+    master_data = SLC_data[master_name]
     for dset_name, dset_data in SLC_data.items():
-            cal_IFG_data[dset_name] = np.empty_like(dset_data)
-            for i in range(dset_data.shape[0]):
-                for j in range(dset_data.shape[1]):
-                    cal_IFG_data[dset_name][i, j] = SLC_data[master_name][i, j] * np.conj(dset_data[i, j])
+        cal_IFG_data[dset_name] = np.multiply(master_data, np.conj(dset_data))
     print("IFG :", cal_IFG_data)
     return cal_IFG_data
 
 
 def write_IFG(cal_IFG_data, hdf5_file_path, group_name):
     with h5py.File(hdf5_file_path, "a") as f:
-        g = f.create_group(group_name)
+        if group_name not in f.keys():
+            g = f.create_group(group_name)
+        else:
+            g = f[group_name]
         for dset_name, dset_data in cal_IFG_data.items():
-            if dset_name not in f[group_name]:
-                f.create_dataset(group_name + '/' + dset_name, data = dset_data)
+            if dset_name not in g.keys():
+                g.create_dataset(dset_name, data = dset_data)
             else:
-                f[group_name + '/' + dset_name][:] = dset_data
+                g[dset_name][:] = dset_data
         f.flush()
         f.close()
 
-def data2jpg(dset):
+def data2jpg(dset, path):
     for dset_name, dset_data in dset.items():
-        # print(dset_name)
-        # print(dset_data)
-        mag_data = np.abs(dset_data)
-        phase_data = np.angle(dset_data)
-        plt.title('Phase Map')
-        plt.figure(dpi=400)
-        plt.imshow(phase_data, cmap='jet')
+                # Convert to complex float array if necessary
+        dset_data = np.asarray(dset_data, dtype=np.complex64)
+
+        # Compute phase map
+        phase = np.angle(dset_data)
+        print(phase[1567][21000:21050])
+        plt.title('phase Map')
+        plt.figure(dpi=200)
+        plt.imshow(phase, cmap='jet')
         plt.colorbar()
-        plt.savefig('/data/tests/hongcan/gecoris_test/HDF5_test/map/cal_DIF/cal_DIF_'+dset_name+'.jpg')
+        plt.savefig(path + '/' + dset_name + '_new_IFG.jpg')
         plt.close()
 
 
 if __name__ == "__main__":
-    data2jpg(read_dset('/data/tests/hongcan/gecoris_test/HDF5_test/stack_s1_asc84.hdf5', 'cal_DIF'))
+    HDF_dataset_path = '/data/tests/hongcan/GF3/cn_inner_mongolia_gf3c_dsc_fsii/gf3.hdf5'
+    jpg_path = '/data/tests/hongcan/GF3/map/IFG'
+    group_name = 'SLC'
+    master_name = '20230119'
+    start_time = time.clock()
+    SLC_data = read_dset(HDF_dataset_path, group_name)
+    read_end_time = time.clock()
+    print("The time required to read data: ", read_end_time - start_time, "s")
+    cal_IFG_data = calculate_IFG(SLC_data, master_name)
+    cal_end_time = time.clock()
+    print("The time required to calculate IFG: ", cal_end_time - read_end_time, "s")
+    write_IFG(cal_IFG_data, HDF_dataset_path, 'cal_IFG')
+    write_end_time = time.clock()
+    print("The time required to write data: ", write_end_time - cal_end_time, "s")
+    data2jpg(cal_IFG_data, jpg_path)
+    jpg_end_time = time.clock()
+    print("The time required to save jpg: ", jpg_end_time - write_end_time, "s")
